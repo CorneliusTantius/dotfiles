2658d8c62961b9387fa63e5171dc88364bf803e5
Cornelius Tantius <corneliustantius.ct@gmail.com>
2026-07-21 09:45:43 +0700

    update

--- cachyos-no-desktop/install-mangowm-noctalia.sh
+++ cachyos-no-desktop/install-mangowm-noctalia.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu
 sudo pacman -S mangowm noctalia --noconfirm
 sudo pacman -S wl-clipboard alacritty xdg-desktop-portal-gnome nautilus --noconfirm
-
+sudo pacman -S ttf-jetbrains-mono-nerd --noconfirm
 mkdir -p ~/.config
 mkdir -p ~/.config/mango
 cp /etc/mango/config.conf ~/.config/mango/
